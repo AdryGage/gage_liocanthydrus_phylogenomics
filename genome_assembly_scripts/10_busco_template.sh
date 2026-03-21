f7dfccf71491af915a2e3f02618ca5806ec5d12f
Adry Gage <adry.gage@lsu.edu>
2026-03-20 19:21:32 -0500

    Added new template script for running BUSCO

---
+++ genome_assembly_scripts/10_busco_template.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -J busco_INDIV      # Job name
+#SBATCH -A hpc_sbaca_su2
+#SBATCH -N 1
+#SBATCH -n 64
+#SBATCH -c 1
+#SBATCH -t 24:00:00
+#SBATCH -p single
+#SBATCH -e ./error_out/_10_busco_INDIV_%j.err        # Error log
+#SBATCH -o ./log_out/_10_busco_INDIV_%j.log     # Standard output
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=adry.gage@lsu.edu
+
+module load conda
+
+conda init
+source activate /work/adry/conda/envs/busco_6.0.0
+
+busco -i /work/adry/processing_room/assemblies/INDIV/contigs.fasta -m genome -o /work/adry/processing_room/assemblies/INDIV/busco/ --auto-lineage --plot /work/adry/processing_room/assemblies/INDIV/busco/ -c 64
